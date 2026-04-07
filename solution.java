05de13f9e87486756549c348e482a6b9519dd14d
Yuvraj Singh <ys792608@gmail.com>
2026-04-07 23:20:29 +0530

    Create solution.java

---
+++ solution.java
@@ -0,0 +1,12 @@
+public class solution {
+    public static void main(String[] args)
+     {
+        double temp = 103.5;
+        if(temp>=100){
+            System.out.println("You have a Fever");
+        }else{
+            System.out.println("dont have a fever");
+        }
+    }
+}
+
